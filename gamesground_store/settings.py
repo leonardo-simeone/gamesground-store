e40e88df8e8507beaa360d232be773b778634e29
Leonardo Simeone <leo_sime@yahoo.com>
2023-07-11 13:35:01 +0000

    Add correct heroku app name to allowed hosts

--- gamesground_store/settings.py
+++ gamesground_store/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-leonardosim-gamesground-vz2zi4ejqx3.ws-eu101.gitpod.io', 'localhost', 'gamesground-store.herokuapp.com']
+ALLOWED_HOSTS = ['8000-leonardosim-gamesground-vz2zi4ejqx3.ws-eu101.gitpod.io', 'localhost', 'gamesground-store-6596e524f29e.herokuapp.com']
 
 
 # Application definition
