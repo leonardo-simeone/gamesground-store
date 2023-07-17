ab54bc70eb91593a98c7e41015d10e7aaa67d4b4
Leonardo Simeone <leo_sime@yahoo.com>
2023-07-17 15:29:39 +0000

    Create add to basket function

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def basket_summary(request):
@@ -7,3 +7,21 @@ def basket_summary(request):
     """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, game_id):
+    """
+    Add quantity of the specified game to the shopping basket
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if game_id in list(basket.keys()):
+        basket[game_id] += quantity
+    else:
+        basket[game_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
