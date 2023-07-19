e44d9c59e1ccb4a1d27d1750bdd83e0ede5e7542
Leonardo Simeone <leo_sime@yahoo.com>
2023-07-19 13:31:47 +0000

    Connect remove from basket url to basket urls

--- basket/urls.py
+++ basket/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.basket_summary, name='basket_summary'),
     path('add/<game_id>/', views.add_to_basket, name='add_to_basket'),
     path('adjust/<game_id>/', views.adjust_basket, name='adjust_basket'),
+    path('remove/<game_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
