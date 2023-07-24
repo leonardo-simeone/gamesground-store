3411859f2167f4241d67ea38017cb30fb253d9c3
Leonardo Simeone <leo_sime@yahoo.com>
2023-07-24 14:40:11 +0000

    Add basic checkout functionality

--- gamesground_store/settings.py
+++ gamesground_store/settings.py
@@ -187,8 +187,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_CHARGE = 7
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'USE_AWS' in os.environ:
     # Cache control
