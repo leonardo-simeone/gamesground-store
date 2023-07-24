567271615bf23baf3213ec69029b450046062557
Leonardo Simeone <leo_sime@yahoo.com>
2023-07-24 12:01:08 +0000

    Add and style stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NE8ZKKjooDQ9vmaZwJKrCUEorVr5NDlnzUVmStcjmYZK954czqVg43qrQTPDJEBekZbCQ5ctIVprNvgassN8eMQ00P7IeHVRY',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
