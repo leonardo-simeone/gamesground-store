477f3583b10e20e817dc5f5e0f8cc3b1ebe2d57b
Leonardo Simeone <leo_sime@yahoo.com>
2023-07-26 15:16:25 +0000

    Connect like game view to games urls

--- games/urls.py
+++ games/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.games, name='games'),
     path('<game_id>', views.game_detail, name='game_detail'),
+    path('like_game/<game_id>', views.like_game, name='like_game'),
 ]
