148643cd94a1f2b55f18bef91ca5bfcf94250957
Leonardo Simeone <leo_sime@yahoo.com>
2023-08-01 15:20:14 +0000

    Connect newsletter view to home urls

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('contact/', views.contact, name='contact'),
     path('contact_list/', views.contact_list, name='contact_list'),
+    path('newsletter/', views.newsletter, name='newsletter'),
 ]
