b0e20e79a8d3d0a70c819aeadd11be966ce3a3d5
Leonardo Simeone <leo_sime@yahoo.com>
2023-08-11 10:14:54 +0000

    Rearrange unit tests for better test procedure

--- home/test_forms.py
+++ home/test_forms.py
@@ -5,10 +5,11 @@ from .forms import *
 class TestContactForm(TestCase):
 
     """
-    We have four test methods: test_form_fields, test_form_placeholders,
-    test_form_label_visibility, and test_form_autofocus.
-    - In test_form_fields, we check if the form fields match the expected
-    field names.
+    We have four test methods: test_form_required_fields,
+    test_form_placeholders, test_form_label_visibility,
+    and test_form_autofocus.
+    - In test_form_required_fields, we check if the form fields
+    match the expected required field names.
     - In test_form_placeholders, we test if the placeholders for each
     form field match the expected values.
     - In test_form_label_visibility, we verify that the labels for
@@ -17,14 +18,26 @@ class TestContactForm(TestCase):
     for the "name" field.
     """
 
-    def test_form_fields(self):
+    def test_form_required_fields(self):
         form = ContactForm()
-        self.assertTrue('name' in form.fields)
-        self.assertTrue('email' in form.fields)
-        self.assertTrue('body' in form.fields)
+        self.assertTrue(form.fields['name'].required)
+        self.assertTrue(form.fields['email'].required)
+        self.assertTrue(form.fields['body'].required)
 
     def test_form_placeholders(self):
         form = ContactForm()
+        self.assertEqual(
+            form.fields['name'].widget.attrs['placeholder'], 'Full Name *'
+            )
+        self.assertEqual(
+            form.fields['email'].widget.attrs['placeholder'],
+            'E-mail address *'
+            )
+        self.assertEqual(
+            form.fields['body'].widget.attrs['placeholder'],
+            'Write your message here *'
+            )
+
         placeholders = {
             'name': 'Full Name *',
             'email': 'E-mail address *',
@@ -32,7 +45,10 @@ class TestContactForm(TestCase):
         }
 
         for field_name, placeholder_text in placeholders.items():
-            self.assertEqual(form.fields[field_name].widget.attrs['placeholder'], placeholder_text)
+            self.assertEqual(
+                form.fields[field_name].widget.attrs['placeholder'],
+                placeholder_text
+                )
 
     def test_form_label_visibility(self):
         form = ContactForm()
@@ -43,15 +59,18 @@ class TestContactForm(TestCase):
     def test_form_autofocus(self):
         form = ContactForm()
         self.assertTrue(form.fields['name'].widget.attrs.get('autofocus'))
+        self.assertIsNone(form.fields['email'].widget.attrs.get('autofocus'))
+        self.assertIsNone(form.fields['body'].widget.attrs.get('autofocus'))
 
 
 class TestNewsletterForm(TestCase):
 
     """
-    We have four test methods: test_form_fields, test_form_placeholders,
-    test_form_label_visibility, and test_form_autofocus.
-    - In test_form_fields, we check if the form fields match the expected
-    field names.
+    We have four test methods: test_form_required_fields,
+    test_form_placeholders, test_form_label_visibility,
+    and test_form_autofocus.
+    - In test_form_required_fields, we check if the form fields
+    match the expected required field names.
     - In test_form_placeholders, we test if the placeholders for each
     form field match the expected values.
     - In test_form_label_visibility, we verify that the labels for
@@ -60,20 +79,31 @@ class TestNewsletterForm(TestCase):
     for the "name" field.
     """
 
-    def test_form_fields(self):
+    def test_form_required_fields(self):
         form = NewsletterForm()
-        self.assertTrue('name' in form.fields)
-        self.assertTrue('email' in form.fields)
+        self.assertTrue(form.fields['name'].required)
+        self.assertTrue(form.fields['email'].required)
 
     def test_form_placeholders(self):
         form = NewsletterForm()
+        self.assertEqual(
+            form.fields['name'].widget.attrs['placeholder'], 'Name *'
+            )
+        self.assertEqual(
+            form.fields['email'].widget.attrs['placeholder'],
+            'E-mail address *'
+            )
+
         placeholders = {
             'name': 'Name *',
             'email': 'E-mail address *',
         }
 
         for field_name, placeholder_text in placeholders.items():
-            self.assertEqual(form.fields[field_name].widget.attrs['placeholder'], placeholder_text)
+            self.assertEqual(
+                form.fields[field_name].widget.attrs['placeholder'],
+                placeholder_text
+                )
 
     def test_form_label_visibility(self):
         form = NewsletterForm()
@@ -83,3 +113,4 @@ class TestNewsletterForm(TestCase):
     def test_form_autofocus(self):
         form = NewsletterForm()
         self.assertTrue(form.fields['name'].widget.attrs.get('autofocus'))
+        self.assertIsNone(form.fields['email'].widget.attrs.get('autofocus'))
