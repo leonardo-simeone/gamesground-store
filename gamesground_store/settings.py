b5e446eb16cd5df1e8433c629ca8ffb56e4f92e0
Leonardo Simeone <leo_sime@yahoo.com>
2023-08-23 09:48:52 +0000

    Add noqa to lines of code that need it in settings

--- gamesground_store/settings.py
+++ gamesground_store/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['8000-leonardosim-gamesground-vz2zi4ejqx3.ws-eu101.gitpod.io', 'localhost', 'gamesground-store-6596e524f29e.herokuapp.com']
+ALLOWED_HOSTS = ['gamesground-store-6596e524f29e.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -98,6 +98,7 @@ TEMPLATES = [
 ]
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -122,6 +123,7 @@ LOGIN_REDIRECT_URL = '/'
 ACCOUNT_FORMS = {
     'signup': 'profiles.forms.CustomSignupForm',
     }
+
 
 WSGI_APPLICATION = 'gamesground_store.wsgi.application'
 
@@ -154,16 +156,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
     },
 ]
 
